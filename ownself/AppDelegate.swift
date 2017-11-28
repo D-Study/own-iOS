4cdf15fd240b708f1a71da8707ddd523ea51a34a
myoung <myoungsc.dev@gmail.com>
2017-11-28 16:59:34 +0900

    first update ownself project

---
+++ ownself/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  ownself
+//
+//  Created by maccli1 on 2017. 11. 28..
+//  Copyright © 2017년 myoung. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        let navigat = UINavigationController()
+        navigat.pushViewController(VcHome(), animated: false)
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+            window.backgroundColor = UIColor.white
+            window.rootViewController = navigat
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) { }
+    func applicationDidEnterBackground(_ application: UIApplication) { }
+    func applicationWillEnterForeground(_ application: UIApplication) { }
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+    func applicationWillTerminate(_ application: UIApplication) { }
+
+}
+
