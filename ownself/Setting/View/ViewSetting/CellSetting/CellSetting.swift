0dc2e568904f43ee42abf8f0cda0cd572fca2d65
myoung <myoungsc.dev@gmail.com>
2017-12-05 18:30:46 +0900

    work setting view

---
+++ ownself/Setting/View/ViewSetting/CellSetting/CellSetting.swift
@@ -0,0 +1,24 @@
+//
+//  CellSetting.swift
+//  ownself
+//
+//  Created by maccli1 on 2017. 12. 5..
+//  Copyright © 2017년 myoung. All rights reserved.
+//
+
+import UIKit
+
+class CellSetting: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ownself/Setting/View/ViewSetting/VcSetting.swift
+++ ownself/Setting/View/ViewSetting/VcSetting.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class VcSetting: UIViewController {
 
+    @IBOutlet weak var tbSetting: UITableView!
     @IBOutlet weak var contNaviHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
@@ -28,6 +29,7 @@ class VcSetting: UIViewController {
     
     //MARK: ## init method ##
     func initNormal() { //init normal setting
+        tbSetting.register(UINib(nibName: "CellSetting", bundle: nil), forCellReuseIdentifier: "cell")
     }
     
     func initAutolayout() { //init autolayout setting
@@ -40,5 +42,35 @@ class VcSetting: UIViewController {
     @IBAction func btnBackB(_ sender: UIButton) { //Back to the Home Page
         self.navigationController?.popViewController(animated: true)
     }
+    
+}
+
+//MARK: ## UITableview delegate and datasource ##
+extension VcSetting: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //갯수
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { //높이
+        return 45
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {  //셀 셋팅
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CellSetting
+        
+        cell.textLabel?.text = "a"
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { //셀선택시
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    
+    
     
 }
