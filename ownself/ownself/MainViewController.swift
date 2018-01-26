62f62d440e9bb19e671765d9f05ad650cde41d9a
myoung <myoungsc.dev@gmail.com>
2018-01-26 12:01:55 +0900

    refresh project, now use MainStoryBoard

---
+++ ownself/ownself/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ownself
+//
+//  Created by maccli1 on 2018. 1. 26..
+//  Copyright © 2018년 myoung. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    
+}
+
+
